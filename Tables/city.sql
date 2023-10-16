ff929f1249cc199188414e3290ccfb5a8a14710b
YULIA VASILENKO <yuliav@devart.com>
2023-10-16 10:42:12 +0300

    modify the column

---
+++ Tables/city.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE `city` (
+  `city_id` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `city` VARCHAR(70) NOT NULL,
+  `country_id` SMALLINT UNSIGNED NOT NULL,
+  `last_update` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (city_id)
+)
+ENGINE = INNODB,
+CHARACTER SET utf8mb4,
+COLLATE utf8mb4_0900_ai_ci;
+
+ALTER TABLE `city` 
+  ADD INDEX idx_fk_country_id(country_id);
+
+ALTER TABLE `city` 
+  ADD CONSTRAINT `fk_city_country` FOREIGN KEY (country_id)
+    REFERENCES country(country_id) ON UPDATE CASCADE;
